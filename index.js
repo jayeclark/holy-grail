62b97f5d6ba9ca2d6c65fad78fe857299e9328e1
Jay Clark <jay@jayeclark.dev>
2021-12-13 17:09:51 -0400

    Refactor redis
    Signed-off-by: Jay Clark <jay@jayeclark.dev>

--- index.js
+++ index.js
@@ -1,61 +1,48 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const redis = require('redis');
+const redis = require("redis");
 const client = redis.createClient();
 client.connect();
 
+const keys = ["header", "left", "article", "right", "footer"];
+
 const setInitialData = async () => {
-  await client
-    .mSet(['header','0','left','0','article','0','right','0','footer','0'], { NX: false })
-    .then(() => {
-      client.mGet(['header', 'left', 'article', 'right', 'footer'])
-            .then(result => console.log(result));
-    })
-}
+  await client.mSet([...keys.map((x) => [x, "0"])]);
+  const result = await client.mGet(keys);
+  console.log(result);
+};
 setInitialData();
 
-function data(){
-  return new Promise((resolve, reject) => {
-    
-    const data = client.mGet(['header', 'left', 'article', 'right', 'footer'])
-      .then((value) => {
-        return {
-          'header': +value[0],
-          'left': +value[1],
-          'article': +value[2],
-          'right': +value[3],
-          'footer': +value[4]
-        }
-      })
+function data() {
+  return new Promise(async (resolve, reject) => {
+    const value = await client.mGet(keys);
+
+    const data = { ...keys };
+    keys.forEach((key, i) => {
+      data[key] = value[i];
+    });
+
     resolve(data);
-  })
+  });
 }
 
-app.use(express.static('client'));
+app.use(express.static("client"));
 
-app.get('/data', (req, res) => {
-  data()
-    .then(data => {
-      res.send(data);
-    })
-})
+app.get("/data", (_, res) => {
+  data().then((data) => {
+    res.send(data);
+  });
+});
 
-app.get('/update/:key/:value', (req, res) => {
-  const key = req.params.key;
-  console.log(key);
-  let newValue = Number(req.params.value);
-  client.get(key).then((oldValue) => {
-    newValue += Number(oldValue);
-    client.set(key, newValue.toString(), { NX: false })
-      .then(() => {
-        data()
-        .then(data => {
-            console.log(data);
-            res.send(data);
-        })
-      });
-  })
-})
+app.get("/update/:key/:value", async (req, res) => {
+  const { key, value } = req.params;
+  const newValue = Number(await client.get(key)) + Number(value);
+  await client.set(key, newValue.toString());
 
-app.listen(3000, () => console.log('Running on port: 3000...'));
+  data().then((data) => {
+    res.send(data);
+  });
+});
+
+app.listen(3000, () => console.log("Running on port: 3000..."));
--- key_get_set.js
+++ key_get_set.js
@@ -3,27 +3,26 @@ const redis = require("redis");
 const client = redis.createClient();
 
 const setData = async () => {
+
   client.connect();
 
   // set and get test key
-  await client.set("my_key", "Hello World").then(() => {
-    client
-      .get("my_key")
-      .then((result) => console.log(result));
-  });
+  await client.set("my_key", "Hello World");
+  const myKey = await client.get("my_key");
+  console.log(myKey);
 
-  // set and get multiple values
-  await client
-    .mSet(
-      ["header", "1", "left", "2", "article", "3", "right", "4", "footer", "5"],
-      { NX: false }
-    )
-    .then(() => {
-      client
-        .mGet(["header", "left", "article", "right", "footer"])
-        .then((result) => console.log(result))
-        .finally(client.quit());
-    });
+  // set multiple values
+  const keys = ["header", "left", "article", "right", "footer"]
+  const keysToSet = [...keys.map(x => [x, "0"])];
+  
+  await client.mSet(keysToSet)
+  
+  // get multiple values and log them to the console
+  const getMultipleKeys = await client.mGet(keys);
+  console.log(getMultipleKeys);
+
+  client.quit();
+
 };
 
 setData();
